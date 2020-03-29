ece420bfc2901a28cc57a0b718d722ef05b58d04
StephArn <34690734+StephArn@users.noreply.github.com>
2020-03-30 00:26:45 +0300

    Final

--- tema1.py
+++ tema1.py
@@ -123,37 +123,95 @@ def testare(lista):
             return 0
     return 1
 
-
-lista=generare_vector(10,100)
+nr_numere=int(input())
+nr_max=int(input())
+lista=generare_vector(nr_numere,nr_max)
 
 l=list(lista)
-
-t1=datetime.datetime.now()
-l.sort()
-t2=datetime.datetime.now()
+if len(l)>10**7:
+    print("Sortarea implicita dureaza prea mult")
+else:
+    t1=datetime.datetime.now()
+    l.sort()
+    t2=datetime.datetime.now()
 
 print("Functia implicita sort: ")
 print(t2-t1)
 
 l=list(lista)
-t1=datetime.datetime.now()
-lb=bubblesort(l)
-t2=datetime.datetime.now()
-print(lb)
+if len(l) > 3000:
+        print("Bubblesort dureaza prea mult")
+else:
+    t1=datetime.datetime.now()
+    lb=bubblesort(l)
+    t2=datetime.datetime.now()
+    if testare(lb)==0:
+        print("Nu e bun")
+    else:
+        print("Functia Bubblesort: ")
+        print(t2-t1)
 
-nr=100
-lc=countsort(l,nr)
+l=list(lista)
+if nr_max > 10 ** 6:
+        print("Countsort ocupa prea multa memorie")
+else:
+    t1=datetime.datetime.now()
+    lc=countsort(l,nr_max)
+    t2=datetime.datetime.now()
+    if testare(lc)==0:
+        print("Nu e bun")
+    else:
+        print("Functia Countsort: ")
+        print(t2-t1)
 
-print(lc)
+l=list(lista)
+if len(l) > 10 ** 6:
+        print("Radixsort cu baza 10 dureaza prea mult")
+else:
+    t1=datetime.datetime.now()
+    lr10=radixsort10(l,nr_max)
+    t2=datetime.datetime.now()
+    if testare(lr10)==0:
+        print("Nu e bun")
+    else:
+        print("Functia Radixsort cu baza 10: ")
+        print(t2-t1)
 
-lr1=radixsort10(l,100)
-print(lr1)
+l=list(lista)
+if len(l) > 10 ** 7:
+        print("Radixsort cu baza 256 dureaza prea mult")
+else:
+    t1=datetime.datetime.now()
+    lr256=radixsort256(l)
+    t2=datetime.datetime.now()
+    if testare(lr256)==0:
+        print("Nu e bun")
+    else:
+        print("Functia Radixsort cu baza 256: ")
+        print(t2-t1)
 
-lr2=radixsort256(l)
-print(lr2)
+l=list(lista)
+if len(l) > 10 ** 6:
+        print("Mergesort dureaza prea mult")
+else:
+    t1=datetime.datetime.now()
+    lm=mergesort(l)
+    t2=datetime.datetime.now()
+    if testare(lm)==0:
+        print("Nu e bun")
+    else:
+        print("Functia Mergesort: ")
+        print(t2-t1)
 
-lm=mergesort(l)
-print(lm)
-
-lq=quicksort(l,0,len(l)-1)
-print(lq)
+l=list(lista)
+if len(l) > 10 ** 6:
+        print("Quicksort dureaza prea mult")
+else:
+    t1=datetime.datetime.now()
+    lq=quicksort(l,0,len(l)-1)
+    t2=datetime.datetime.now()
+    if testare(lq)==0:
+        print("Nu e bun")
+    else:
+        print("Functia Quicksort: ")
+        print(t2-t1)
